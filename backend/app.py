ec4c99e1f1cea752838801054b12645ce5fbb562
Daria Dobrolinski <dsdobrolinski@gmail.com>
2025-05-31 18:03:30 -0400

    implement user authentication with JWT and FastAPI; add login and token verification endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,83 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Request, status
 from pydantic import BaseModel
 from typing import Dict, List, Optional
+from pymongo import MongoClient
+import os
 
 from utils.symptoms import extract
 from utils.classification import classifyCondition
 from utils.recommender import bestPlant
 from utils.recipe import getRecipe
 
-#Load environment variables if needed
 from dotenv import load_dotenv
 load_dotenv('.env')
+
+port = 8000
+client = MongoClient(os.getenv("MONGODB_URI"), port)
+db = client["User"]
+
+from auth.hashing import Hash
+from auth.oauth import getCurrentUser
+from auth.jwttoken import createAccessToken
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+class User(BaseModel):
+    username: str
+    password: str
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    accessToken: str
+    tokenType: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+@app.get("/")
+def readRoot(currentUser: User = Depends(getCurrentUser)):
+    return {"data": "Welcome to Elara"}
+
+# @app.post("/register")
+# def createUser(request: User):
+#     hashedPassword = Hash.bcrypt(request.password)
+#     userObject = dict(request)
+#     userObject["password"] = hashedPassword
+#     userID = db["users"].insert_one(userObject)
+#     return {"res": "created"}
+
+@app.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = db["users"].find_one({"username": form_data.username})
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    
+    if not Hash.verify(user["password"], form_data.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password"
+        )
+    
+    accessToken = createAccessToken(data={"sub": user["username"]})
+    return {"access_token": accessToken, "token_type": "bearer"}
 
 class RecReq(BaseModel):
     medicalConcern: str
@@ -26,9 +94,8 @@ class PlantInfo(BaseModel):
 class RecResp(BaseModel):
     output: Dict[str, Optional[PlantInfo]]
 
-app = FastAPI()
 @app.post("/getRecommendations", response_model=RecResp)
-async def getRecommendations(req: RecReq):
+async def getRecommendations(req: RecReq, currentUser: User = Depends(getCurrentUser)):
     rawSymptoms = extract(req.medicalConcern)
     symptomsList = rawSymptoms["symptoms"]
 
@@ -53,7 +120,7 @@ class RecipeResp(BaseModel):
     output: RecipeData
 
 @app.post("/getRecipe", response_model = RecipeResp)
-async def recipe(req: RecipeReq):
+async def recipe(req: RecipeReq, currentUser: User = Depends(getCurrentUser)):
     recipeDict = getRecipe(
         req.plantName,
         req.scientificName,
